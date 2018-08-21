315549a28a4896f7cfeb8002d81659b49aeb94f3
Thomas Osterbind <tom@qri.io>
2018-08-21 18:21:40 -0400

    updated to support both csv and json

--- qri/client.py
+++ qri/client.py
@@ -83,37 +83,67 @@ class QriDataset(object):
   def _update_head_from_body(self):
     pass
   
-  def save(self, commit_msg):
+  def save(self, commit_msg, publish=True):
     tmp_body_save_path = os.path.join(_TMP_PATH, "body.json")
     tmp_head_save_path = os.path.join(_TMP_PATH, "head.json") 
+    if is_csv(self.head):
+      tmp_body_save_path = tmp_body_save_path.replace(".json", ".csv")
     body = self.body
     head = self.head
     head['bodyPath'] = tmp_body_save_path
     name = self.ds_name
     head['name'] = name
-    with open(tmp_body_save_path, "w") as fp:
-      d = body.where((pd.notnull(body)), None).to_dict(orient="records")
-      json.dump(d, fp, indent=2)
-      # fp.write(json.dumps(body.to_dict(orient="records"), indent=2))
+    # save body to temp file
+    if is_csv(self.head):
+      d = body.where((pd.notnull(body)), None).to_csv(tmp_body_save_path, index=False)
+    else:
+      with open(tmp_body_save_path, "w") as fp:
+        d = body.where((pd.notnull(body)), None).to_dict(orient="records")
+        json.dump(d, fp, indent=2)
+    # save head to temp file
     with open(tmp_head_save_path, "w") as fp:
       json.dump(head, fp, indent=2)
     flags = { "head_path": tmp_head_save_path,
               "commit_msg": commit_msg,
               "name": name,
+              "publish": "-p " if publish else "", 
             }
-    cmd = "qri save -p --file '{head_path}' -t '{commit_msg}' {name}".format(**flags)
+
+    cmd = "qri save {publish}--file '{head_path}' -t '{commit_msg}' {name}".format(**flags)
     result = _shell_exec(cmd)
     print(result)
     # remove tmp files
-    clean_up_files([tmp_body_save_path, tmp_head_save_path])
+    # clean_up_files([tmp_body_save_path, tmp_head_save_path])
     print("dataset saved")
 
+def is_csv(head):
+  if 'structure' in head and 'format' in head['structure'] and head['structure']['format'].lower() == "csv":
+    return True
+  else:
+    return False
+
+def convert_csv(body):
+  field_names = body[0]
+  data = body[1:]
+  # print("fieldnames: type: {}, len:{}".format(type(field_names)), len(field_names))
+  # print("data: type: {}, len:{}".format(type(data)), len(data))
+
+  new_data = list()
+  for row in data:
+    d = dict()
+    for key, value in zip(field_names, row):
+      d[key] = value
+    new_data.append(d)
+  return new_data
+
 #---------------------------------------------------------------------
-def _load_ds_body(name):
+def _load_ds_body(name, fix_csv=False):
   flags = {"name": name}
   cmd = "qri body -a {name}".format(**flags)
   result = _shell_exec(cmd)
   data = json.loads(result)
+  if fix_csv:
+    data = convert_csv(data)
   return pd.DataFrame.from_records(data)
 
 def _load_ds_head(name):
@@ -126,8 +156,12 @@ def _load_ds_head(name):
 def load_ds(ds_name):
   if len(ds_name.split("/")) != 2:
     ds_name = u"me/{}".format(ds_name)
-  body = _load_ds_body(ds_name)
   head = _load_ds_head(ds_name)
+  if is_csv(head):
+    body = _load_ds_body(ds_name, fix_csv=True)
+  else:
+    body = _load_ds_body(ds_name, fix_csv=False)
+
   return QriDataset(body, head, ds_name)
 
 #---------------------------------------------------------------------

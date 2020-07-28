501370a1728e55641bbce0cdd2326464297052b2
Dustin Long <me@dustmop.io>
2020-07-28 15:19:16 -0400

    Better error message if qri cmdline is not installed

--- qri/cmd_util.py
+++ qri/cmd_util.py
@@ -1,17 +1,24 @@
 import re
 import shlex
 from subprocess import Popen, PIPE
+import sys
 
 
 def shell_exec(command, cwd=None):
   """execute commands and return stdout"""
-  proc = Popen(shlex.split(command),
-               stdin=PIPE,
-               stdout=PIPE,
-               stderr=PIPE,
-               cwd=cwd)
-  stdout, err = proc.communicate()
-  return stdout, err
+  try:
+    proc = Popen(shlex.split(command),
+                 stdin=PIPE,
+                 stdout=PIPE,
+                 stderr=PIPE,
+                 cwd=cwd)
+    stdout, err = proc.communicate()
+    return stdout, err
+  except FileNotFoundError as e:
+    sys.stderr.write("""qri command-line binary not found. It is either not installed, or PATH needs to be assigned. Please get the latest release from https://github.com/qri-io/qri, then run this command again.\n""")
+    sys.exit(1)
+  except:
+    raise e
 
 
 def strip_color(colored_text):
